9126077f2d2bb7f06a8fddbc5c63c0b3d1bbed79
Karim A C <mail@ackarim.com>
2015-12-15 16:58:29 +0000

    adding sample of changing env

--- exec/changing-env/changing-env.js
+++ exec/changing-env/changing-env.js
@@ -1,21 +1,22 @@
-var env = process.env,
+var environment = process.env,
   //  someVar,
     envDup = {},
     child_process = require('child_process');
 
 // Duplicate the parent's environment object
-for (someVar in env) {
+/*for (someVar in env) {
     envDup[someVar] = env[somevar];
-}
+}*/
+envDup = environment;
 
 // Now, extend this with some new variables:
 envDup['VAR NAME'] = 'var value';
+envDup['HOME'] = '/';
 
-console.log(env);
 //console.log(envDup);
 
 // Run child process with these environment variables
-child_process.exec('ls',
+child_process.exec('ls -l $HOME',
     { env: envDup},
     function (err, stdout, stderr) {
         if (err) {
